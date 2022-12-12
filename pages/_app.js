6a20bd50cd55103c32ac115e38f770f3bf04c173
Bastian Kroggel <b.kroggel@yahoo.de>
2022-12-12 13:49:57 +0100

    initial setup of expo2023 placeholder

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "sanitize.css";
+import "sanitize.css/typography.css";
+import localFont from "@next/font/local";
+
+const PPNeueMachina = localFont({
+  src: "../fonts/PPNeueMachina-InktrapRegular.woff2",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} className={PPNeueMachina.className} />;
 }
 
-export default MyApp
+export default MyApp;
